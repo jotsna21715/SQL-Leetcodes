aa276282c1f1511721193121e32e20c93f4ef1d8
jotsna21715 <jotsnasriy@gmail.com>
2024-10-08 17:48:45 -0500

    Solution

---
+++ Average Selling/Average Selling Price.sql
@@ -0,0 +1,6 @@
+SELECT p.product_id,ROUND(SUM(u.units * p.price) / SUM(u.units),2) AS average_price
+FROM UnitsSold u 
+INNER JOIN Prices p 
+    ON u.product_id = p.product_id 
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date 
+GROUP BY p.product_id;
